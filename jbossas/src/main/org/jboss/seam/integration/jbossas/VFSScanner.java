8285bd8031bbd1c9dd31ad5450e9aa671deffb49
Pete Muir <pete.muir@jboss.org>
2008-08-18 12:55:07 +0000

    Add VFSScanner
    git-svn-id: https://anonsvn.jboss.org/repos/jbossas/projects/jboss-seam-int/trunk@77156 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ jbossas/src/main/org/jboss/seam/integration/jbossas/VFSScanner.java
@@ -0,0 +1,171 @@
+package org.jboss.seam.integration.jbossas;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.List;
+
+import org.jboss.seam.deployment.AbstractScanner;
+import org.jboss.seam.deployment.DeploymentStrategy;
+import org.jboss.seam.log.LogProvider;
+import org.jboss.seam.log.Logging;
+import org.jboss.virtual.VFS;
+import org.jboss.virtual.VirtualFile;
+
+/**
+ * JBoss VSF aware scanner.
+ *
+ * @author <a href="mailto:ales.justin@jboss.com">Ales Justin</a>
+ */
+public class VFSScanner extends AbstractScanner
+{
+   private static final LogProvider log = Logging.getLogProvider(VFSScanner.class);
+
+   public VFSScanner(DeploymentStrategy deploymentStrategy)
+   {
+      super(deploymentStrategy);
+   }
+
+   /**
+    * Get the virtual file root.
+    *
+    * @param url the root URL
+    * @param parentDepth level of parent depth
+    * @return actual virtual file from url param
+    * @throws IOException for any error
+    */
+   protected static VirtualFile getRoot(URL url, int parentDepth) throws IOException
+   {
+
+      log.trace("Root url: " + url);
+
+      String urlString = url.toString();
+      // TODO - this should go away once we figure out why -exp.war is part of CL resources
+      if (urlString.startsWith("vfs") == false)
+         return null;
+
+      int p = urlString.indexOf(":");
+      String file = urlString.substring(p + 1);
+      URL vfsurl = null;
+      String relative;
+      File fp = new File(file);
+      
+      log.trace("File: " + fp);
+
+      if (fp.exists())
+      {
+         vfsurl = fp.getParentFile().toURL();
+         relative = fp.getName();
+      }
+      else
+      {
+         File curr = fp;
+         relative = fp.getName();
+         while ((curr = curr.getParentFile()) != null)
+         {
+            if (curr.exists())
+            {
+               vfsurl = curr.toURL();
+               break;
+            }
+            else
+            {
+               relative = curr.getName() + "/" + relative;
+            }
+         }
+      }
+
+      log.trace("URL: " + vfsurl + ", relative: " + relative);
+
+      VirtualFile top = VFS.getRoot(vfsurl);
+      top = top.getChild(relative);
+      while(parentDepth > 0)
+      {
+         if (top == null)
+            throw new IllegalArgumentException("Null parent: " + vfsurl + ", relative: " + relative);
+         top = top.getParent();
+         parentDepth--;
+      }
+
+      log.trace("Top: " + top);
+
+      return top;
+   }
+
+   public void scanDirectories(File[] directories)
+   {
+      for (File dir : directories)
+      {
+         try
+         {
+            VirtualFile root = getRoot(dir.toURL(), 0);
+            if (root != null)
+               handleRoot(root);
+            else
+               log.trace("Null root: " + dir);
+         }
+         catch (IOException e)
+         {
+            log.warn("Cannot scan directory " + dir, e);
+         }
+      }
+   }
+
+   public void scanResources(String[] resources)
+   {
+      for (String resourceName : resources)
+      {
+         try
+         {
+            Enumeration<URL> urlEnum = getDeploymentStrategy().getClassLoader().getResources(resourceName);
+            while (urlEnum.hasMoreElements())
+            {
+               URL url = urlEnum.nextElement();
+               VirtualFile root = getRoot(url, resourceName.lastIndexOf('/') > 0 ? 2 : 1);
+               if (root != null)
+                  handleRoot(root);
+               else
+                  log.trace("Null root: " + url);
+            }
+         }
+         catch (IOException ioe)
+         {
+            log.warn("Cannot read resource: " + resourceName, ioe);
+         }
+      }
+   }
+
+   /**
+    * Handle virtual file root.
+    *
+    * @param root the virtual file root
+    * @throws IOException for any error
+    */
+   protected void handleRoot(VirtualFile root) throws IOException
+   {
+      if (root.isLeaf())
+      {
+         getDeploymentStrategy().handle(root.getPathName());
+      }
+      else
+      {
+         String rootPathName = root.getPathName();
+         int rootPathNameLength = rootPathName.length();
+         List<VirtualFile> children = root.getChildrenRecursively();
+         for (VirtualFile child : children)
+         {
+            if (child.isLeaf())
+            {
+               String name = child.getPathName();
+               // move past '/'
+               int length = rootPathNameLength;
+               if (name.charAt(length) == '/')
+                  length++;
+               getDeploymentStrategy().handle(name.substring(length));
+            }
+         }
+      }
+   }
+}
